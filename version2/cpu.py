755560b21c323f077f89e17ed3c38775c4ff9061
m47eus2 <mateusz.ciesielski2003@gmail.com>
2025-07-15 18:42:19 +0200

    Stretching width, constant height, more options in selector

--- version2/cpu.py
+++ version2/cpu.py
@@ -1,4 +1,4 @@
-from bokeh.layouts import column
+from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, Select
 from bokeh.plotting import figure, curdoc
 import pandas as pd
@@ -9,14 +9,16 @@ sample_count = {'value':60}
 
 source = ColumnDataSource(data=dict(x=[], y=[]))
 
-p=figure(title='CPU temp', x_axis_type="datetime")
+p=figure(title='CPU temp', x_axis_type="datetime", height=800)
 p.line(x='x', y='y', source=source, color="red", line_width=2)
-p.sizing_mode = "scale_width"
-p.height=300
+p.sizing_mode = "stretch_width"
+
 
 select = Select(title='Zakres danych',value='60',
                 options=[
                   ('60', '60 (1 min)'),
+                  ('120', '120 (2min)'),
+                  ('180', '180 (3min)'), 
                   ('300', '300 (5 min)'),
                   ('600', '600 (10 min)'),
                   ('1800', '1.8k (30 min)')])
@@ -32,7 +34,9 @@ def update():
   data['time'] = pd.to_datetime(data['time'])
   source.data = dict(x=data['time'], y=data['cpuTemp'])
 
-curdoc().add_root(column(p, select, sizing_mode="scale_width"))
+layout = column(p,select,sizing_mode='stretch_width')
+
+curdoc().add_root(layout)
 curdoc().add_periodic_callback(update, 1000)
 
 #run with: bokeh serve cpu.py --port 5000 --allow-websocket-origin=192.168.x.x:5000
